ec0280a7f608061db4d7806d7d9a8bd65bbaebed
CHOUCHENAhmed <120291458+CHOUCHENAhmed@users.noreply.github.com>
2022-12-26 13:07:06 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, iam Ahmed CHOUCHEN!");
+        System.out.println("Hello, CHOUCHEN Ahmed!");
     }
 }
 
